0f8eeffcb905ced681cb28b89afcb57a04433a77
navendu10 <navendu.vyas10@gmail.com>
2024-06-20 15:43:34 +0530

    Resolved merge conflicts

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ st.set_page_config(layout="wide")
 col1, col2 = st.columns(2)
 
 with col1:
-    st.image("images/photo.jpg")
 
 with col2:
     st.title("Navendu Vyas")
@@ -22,18 +21,15 @@ st.write(content2)
 
 col3, empty_col, col4 = st.columns([1.5,0.5,1.5])
 
-df = pd.read_csv("data.csv", sep=";")
 
 with col3:
     for index,row in df[:10].iterrows():
         st.header(row["title"])
         st.write(row["description"])
-        st.image("images/" + row["image"])
         st.write(f"[Source Code]({row['url']})")
 
 with col4:
     for index, row in df[10:].iterrows():
         st.header(row["title"])
         st.write(row["description"])
-        st.image("images/" + row["image"])
         st.write(f"[Source Code]({row['url']})")
