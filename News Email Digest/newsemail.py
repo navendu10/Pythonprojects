3762e6c9b239b5d1c110df4486349e97eadbb813
navendu10 <navendu.vyas10@gmail.com>
2024-07-18 13:42:54 +0530

    Configure email send option with title

--- News Email Digest/newsemail.py
+++ News Email Digest/newsemail.py
@@ -1,4 +1,5 @@
 import requests
+from send_email import send_email
 
 api_key = "3cbcc2cd668142f6bad98be3164b47a1"
 url = "https://newsapi.org/v2/everything?q=tesla&from=2024-06-18&sortBy=publishedAt&apiKey=3cbcc2cd668142f6bad98be3164b47a1"
@@ -10,6 +11,11 @@ request = requests.get(url)
 content = request.json()
 
 # Access the articles title and description
+body = ""
 for article in (content["articles"]):
-    print(article["title"])
-    print(article["Description"])
+    title = article.get("title", "No title")
+    description = article.get("description", "No description")
+    body += f"{title}\n{description}\n\n"
+
+body = body.encode("utf-8")
+send_email(message=body)
