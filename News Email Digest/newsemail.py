3ac3dad5807ddd832bad493d93160af68bd8f19c
navendu10 <navendu.vyas10@gmail.com>
2024-07-19 14:45:33 +0530

    added Email Subject, topic for filtering

--- News Email Digest/newsemail.py
+++ News Email Digest/newsemail.py
@@ -1,21 +1,26 @@
 import requests
 from send_email import send_email
 
+topic = "tesla"
 api_key = "3cbcc2cd668142f6bad98be3164b47a1"
-url = "https://newsapi.org/v2/everything?q=tesla&from=2024-06-18&sortBy=publishedAt&apiKey=3cbcc2cd668142f6bad98be3164b47a1"
+url = ("https://newsapi.org/v2/everything?"
+       f"q={topic}&from=2024-06-18&sortBy=publishedAt&apiKey=3cbcc2cd668142f6bad98be3164b47a1$language=en")
+response = requests.get(url)
 
 # Make request
 request = requests.get(url)
+body = "Subject: Today's news\n"
+if response.status_code == 200:
+    content = request.json()
 
-# Get a dictionary with data
-content = request.json()
+    if 'articles' in content:
 
-# Access the articles title and description
-body = ""
-for article in (content["articles"]):
-    title = article.get("title", "No title")
-    description = article.get("description", "No description")
-    body += f"{title}\n{description}\n\n"
+        for article in content["articles"][:20]:
+            title = article["title"] if article["title"] is not None else "No title"
+            description = article["description"] if article["description"] is not None else "No description"
+            url = article["url"] if article["url"] is not None else "No URL"
+            body += f"{title}\n{description}\n{url}\n\n"
+
 
 body = body.encode("utf-8")
 send_email(message=body)
